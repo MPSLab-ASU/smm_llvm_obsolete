df5bb7576d0aefade5e246ada87ff9de17aaba5a
Jian Cai <jcai19@asu.edu>
2015-04-02 13:58:16 -0700

    Fix bugs in l2g insertion and sstore & sload insertion in stack management pass

--- lib/Transforms/StackManagement/StackManagement.cpp
+++ lib/Transforms/StackManagement/StackManagement.cpp
@@ -42,7 +42,7 @@ namespace {
 	}
 
 	// Check if a function is stack management function	
-	inline bool isManagementFunction(Function *func) {
+	inline bool isStackManagementFunction(Function *func) {
 	    if (func->getName().count("_g2l") ==1)
 		return true;
 	    if (func->getName().count("_l2g") ==1)
@@ -97,7 +97,7 @@ namespace {
 	    InlineAsm *func_putSP = InlineAsm::get(functy_inline_asm, "mov $0, %rsp;", "*m,~{rsp},~{dirflag},~{fpsr},~{flags}",true);
 	    InlineAsm *func_getSP = InlineAsm::get(functy_inline_asm, "mov %rsp, $0;", "=*m,~{dirflag},~{fpsr},~{flags}",true);
 
-	    // Step 1: transform the main function to an user function
+	    // Step 1: transform the main function to an user-defined function
 	    // Create an external function called smm_main
 	    Function *func_smm_main = Function::Create(cast<FunctionType>(func_main->getType()->getElementType()), func_main->getLinkage(), "smm_main", &mod);
 	    ValueToValueMapTy VMap;
@@ -152,24 +152,27 @@ namespace {
 	    // Step 2 : Add noinline attributes to functions
 	    for (Module::iterator fi = mod.begin(), fe = mod.end(); fi != fe; ++fi) {
 		if (fi->hasFnAttribute(Attribute::NoInline) || fi->hasFnAttribute(Attribute::AlwaysInline) )
-			continue;
+		    continue;
 		fi->addFnAttr(Attribute::NoInline);
 	    }
 
 
-	    // Step 3: Transform the functions with address arguments to pass pointers instead
+	    // Step 3: Insert l2g functions
 	    for (Module::iterator fi = mod.begin(), fe = mod.end(); fi != fe; ++fi) {
+		
+		//errs() << fi->getName() << "\n";
+
 		Function *caller = &*fi;
 		// Skip if it is a library function
 		if (isLibraryFunction(caller))
 		    continue;
 		// Skip if it is a stack management function
-		if (isManagementFunction(caller))
+		if (isStackManagementFunction(caller))
 		    continue;
 		// Skip if it is main function
 		if (&*fi == func_main)
 		    continue;
-		// We have found an user function (including main function)
+		// We have found an user-defined function (including main function)
 		for (Function::iterator bi = fi->begin(), be = fi->end(); bi != be; ++bi) {
 		    for (BasicBlock::iterator ii = bi->begin(), ie = bi->end(); ii != ie; ii++) {
 			Instruction *inst = ii;
@@ -178,97 +181,36 @@ namespace {
 			    if (call_func->isInlineAsm())
 				continue;
 			    Function *callee = call_func->getCalledFunction();
-			    //Skip if a stack management function is called
-			    if (isManagementFunction(callee))
-				continue;
+			    // If the callee is a function pointer, skip the check and insert management functions
+			    if (callee) { // If callee is not a function pointer
+				//Skip if a stack management function is called
+				if (isStackManagementFunction(callee))
+				    continue;
+				//Skip if an intrinsic function is called
+				if (callee->isIntrinsic())
+				    continue;
+			    } 
 			    for (unsigned int i = 0, n = call_func->getNumArgOperands(); i < n; i++) { // We have found function calls wth address arguments
 				Value *operand = call_func->getArgOperand(i);
 				if (operand->getType()->isPointerTy() ) {
-				    // Create a pointer variable for each address argument in global space
-				    GlobalVariable *ptr = new GlobalVariable(mod, //Module
-					    operand->getType(), //Type
-					    false, //isConstant
-					    GlobalValue::ExternalLinkage, //linkage
-					    0, // Initializer
-					    "_ptr"); //Name
-				    // Initialize the temporary global variable
-				    ptr->setInitializer(Constant::getNullValue(operand->getType()));
-				    // Assign the pointer with the address
-				    new StoreInst(operand, ptr, inst);
-				    // Replace the address with corresponding pointer
-				    LoadInst *ptrval = new LoadInst(ptr, "", inst);
-				    call_func->setOperand(i, ptrval);
+				    IRBuilder<> builder(call_func); // Instruction will be inserted before ii
+				    Value *cast_to = builder.CreatePointerCast(operand, Type::getInt8PtrTy(context), "cast_to_char_pointer"); // Cast the value (in this case, a memory address) to be of char pointer type required by l2g function
+				    Value *call_l2g = builder.CreateCall(func_l2g, cast_to, "l2g_on_char_pointer"); // Call the function l2g with the value with cast type
+				    Value *cast_from = builder.CreatePointerCast(call_l2g, operand->getType(), "cast_from_result"); // Cast the result back to be of the original type
+				    for (unsigned int i = 0; i < call_func->getNumOperands(); i++) {
+					if (call_func->getOperand(i) == operand ) // Replace the use of the original memory address with the translated address
+					    call_func->setOperand(i, cast_from); // Replace the use of the original memory address with the translated address
+				    }
 				}
 			    }
+
 			}
 		    }
 		}
 	    }
 
-	    // Step 4: Insert l2g and g2l function calls
-	    for (Module::iterator fi = mod.begin(), fe = mod.end(); fi != fe; ++fi) {
-		Function *caller = &*fi;
-		// Skip if it is a library function
-		if (isLibraryFunction(caller))
-		    continue;
-		// Skip if it is a stack management function
-		if (isManagementFunction(caller))
-		    continue;
-		// Skip if it is a main function
-		if (&*fi == func_main)
-		    continue;
-		// We have found an user function
-		for (Function::iterator bi = fi->begin(), be = fi->end(); bi != be; ++bi) {
-		    for (BasicBlock::iterator in=bi->begin(), ii=in++, ie = bi->end(); ii != ie; ii=in++) {
-			Instruction *inst = ii;
-			Instruction *next_inst = in;
+	    // TODO: Step 4: Insert g2l function calls
 
-			// Identify definition of pointers
-			if (inst->getOpcode() == Instruction::Store) {
-			    Value *operand0 = inst->getOperand(0);
-			    Value *operand1 = inst->getOperand(1);
-			    if (operand1->getType()->isPointerTy() && operand1->getType()->getPointerElementType()->isPointerTy()) {
-				//We have found a pointer to stack data on the LHS, we need to wrap the RHS in a l2g function call.  The RHS is not a function argument.
-				IRBuilder<> builder(ii); // Instruction will be inserted before ii
-				// Take the data on the RHS and cast it to void * so it can be passed ot our function l2g
-				Value *castToCharStar = builder.CreatePointerCast(operand0, Type::getInt8PtrTy(context), "castToChar*");
-				// Call the function l2g and pass the void * data as a parameter to the function
-				Value *call_l2g = builder.CreateCall(func_l2g, castToCharStar, "call_l2g");
-				// Take the result from the function call and cast it to its previous type.
-				Value *castFromCharStar = builder.CreatePointerCast(call_l2g, operand0->getType(), "castFromChar*");
-				// Assign the result to the LHS of the original instruction.
-				builder.CreateStore(castFromCharStar, operand1);
-				// Delete the original instruction.
-				//(ii++)->eraseFromParent();                                                              
-				ii->eraseFromParent();                                                              
-			    }
-			}
-			// Identify deference of pointers
-			else if (inst->getOpcode() == Instruction::Load) {
-			    Value *operand0 = inst->getOperand(0);
-			    if (operand0->getType()->isPointerTy() && operand0->getType()->getPointerElementType()->isPointerTy()) {
-				if ( next_inst->getOpcode() == Instruction::Load && next_inst->getOperand(0) == inst) {
-				    // We have found a dereference of a pointer.  The next instruction is a load instruction and the current instructions value is the operand for the next instruction.
-				    //errs() << fi->getName() << ": " << inst << " [" << *inst << "]\n\t is used in next instruction:[" << *next_inst <<"] as operand 0 " << next_inst->getOperand(0) << " ("<< *next_inst->getOperand(0) <<")\n";
-				    IRBuilder<> builder(next_inst);
-				    Value *castToCharStar = builder.CreatePointerCast(inst, Type::getInt8PtrTy(context), "castToChar*");
-				    Value *call_g2l = builder.CreateCall(func_g2l, castToCharStar, "call_g2l");
-				    Value *castFromCharStar = builder.CreatePointerCast(call_g2l, next_inst->getOperand(0)->getType(), "castFromChar*");
-				    next_inst->setOperand(0, castFromCharStar);
-				} else if (next_inst->getOpcode() == Instruction::Store && next_inst->getOperand(1) == inst) {
-				    // We have found a dereference of a pointer.  The next instruction is a store instruction and the current instructions value is the operand for the next instruction.
-				    //errs() << fi->getName() << ": " << inst << " [" << *inst << "]\n\t is used in next instruction:[" << *next_inst <<"] as operand 1 " << next_inst->getOperand(1) << " ("<< *next_inst->getOperand(1) <<")\n";
-				    IRBuilder<> builder(next_inst);
-				    Value *castToCharStar = builder.CreatePointerCast(inst, Type::getInt8PtrTy(context), "castToChar*");
-				    Value *call_g2l = builder.CreateCall(func_g2l, castToCharStar, "call_g2l");
-				    Value *castFromCharStar = builder.CreatePointerCast(call_g2l, next_inst->getOperand(1)->getType(), "castFromChar*");
-				    next_inst->setOperand(1, castFromCharStar);
-				}
-			    }
-			}
-		    }
-		}
-	    }
 
 	    // Step 5: Insert management functions
 	    for (Module::iterator fi = mod.begin(), fe = mod.end(); fi != fe; ++fi) {
@@ -277,12 +219,12 @@ namespace {
 		if (isLibraryFunction(caller))
 		    continue;
 		// Skip if it is a stack management function
-		if (isManagementFunction(caller))
+		if (isStackManagementFunction(caller))
 		    continue;
 		// Ignore if it is main function
 		if (&*fi == func_main)
 		    continue;
-		// We have found an user function (including main function)
+		// We have found an user-defined function (including main function)
 		for (inst_iterator in = inst_begin(fi), ii=in++, ie = inst_end(fi); ii != ie; ii=in++) {
 		    Instruction *inst = &*ii;
 		    Instruction *next_inst = &*in;
@@ -291,10 +233,15 @@ namespace {
 			if (call_func->isInlineAsm())
 			    continue;
 			Function *callee = call_func->getCalledFunction();
-			// Skip if a management function is called
-			if (isManagementFunction(callee))
-			    continue;
-			errs() << caller->getName() << " calls " << callee->getName() << "\n";
+			// If the callee is a function pointer, skip the check and insert management functions
+			if(callee) { // The callee is not a function pointer
+			    // Skip if a management function is called
+			    if (isStackManagementFunction(callee))
+				continue;
+			    // Skip if an llvm intrinsic is called
+			    if (callee->isIntrinsic())
+				continue;
+			} 
 
 			// Before the function call
 			//	Insert a sstore function
@@ -328,47 +275,46 @@ namespace {
 			if (retty->isVoidTy())
 			    continue;
 			// Save return value in a global variable temporarily until sload is executed if it is used
-			unsigned int i, n;
 			GlobalVariable *gvar = NULL; // Always create a new global variable in case of recursive functions
-			for (inst_iterator ij = in; ij != ie; ij++) {
-			    Instruction *sub_inst = &*ij;
-			    for (i = 0, n = sub_inst->getNumOperands(); i < n; i++) {
-				if (sub_inst->getOperand(i) == inst) { // We have found the use of return value
-				    if (!gvar) {
-					gvar = new GlobalVariable(mod, //Module
-						retty, //Type
-						false, //isConstant
-						GlobalValue::ExternalLinkage, //linkage
-						0, // Initializer
-						"_gvar"); //Name
-					// Initialize the temporary global variable
-					gvar->setInitializer(Constant::getNullValue(retty));
-					// Save return value to the global variable before sload is called
-					StoreInst *st_ret = new StoreInst(call_func, gvar);
-					st_ret->insertAfter(inst);
-				    }
 
-				    if (PHINode *target = dyn_cast<PHINode>(sub_inst)) { // Return value is used in a phi instruction
-					for (unsigned i = 0; i < target->getNumIncomingValues(); i++) {
-					    if(dyn_cast<CallInst>(target->getIncomingValue(i))) {
-						//BasicBlock *bb = target->getIncomingBlock(i);
-						//errs() << "Before: " << fi->getName() << " : " << target->getParent()->getName() << " : "<< *target << "\n";
-						//errs() << "\t" << bb->getName() << " : " << *target->getIncomingValue(i) << "\n";
-						// Read the global variable
-						LoadInst *restore_ret = new LoadInst(gvar, "", target->getIncomingBlock(i)->getTerminator());
-						// Use read value
-						sub_inst->setOperand(i, restore_ret);
-						//errs() << "After: "<< fi->getName() << " : " << target->getParent()->getName() << " : "<< *target << "\n";
-						//errs() << "\t" << bb->getName() << " : " << *target->getIncomingValue(i) << "\n";
+			for (Value::user_iterator ui_ret = call_func->user_begin(), ue_ret = call_func->user_end(); ui_ret != ue_ret; ++ui_ret) {
+			    if (Instruction *user_inst = dyn_cast<Instruction>(*ui_ret)) { // We have found the use of return value
+				if (!gvar) {
+				    gvar = new GlobalVariable(mod, //Module
+					    retty, //Type
+					    false, //isConstant
+					    GlobalValue::ExternalLinkage, //linkage
+					    0, // Initializer
+					    "_gvar"); //Name
+				    // Initialize the temporary global variable
+				    gvar->setInitializer(Constant::getNullValue(retty));
+				    // Save return value to the global variable before sload is called
+				    StoreInst *st_ret = new StoreInst(call_func, gvar);
+				    st_ret->insertAfter(call_func);
+				}
+
+				if (PHINode *target = dyn_cast<PHINode>(user_inst)) { // Return value is used in a phi instruction
+				    for (unsigned int i = 0; i < target->getNumIncomingValues(); i++) {
+					if(target->getIncomingValue(i) == call_func) {
+					    // Read the global variable
+					    LoadInst *restore_ret = new LoadInst(gvar, "", target->getIncomingBlock(i)->getTerminator());
+					    // Find the use of return value and replace it (At most one use in phi instruction)
+					    for (unsigned int j = 0; j < user_inst->getNumOperands(); j++) {
+						if (user_inst->getOperand(j) == call_func) {
+						    user_inst->setOperand(j, restore_ret);
+						}
 					    }
-
 					}
 
-				    } else { // Return value is used in a non-phi instruction
-					// Read the global variable
-					LoadInst *restore_ret = new LoadInst(gvar, "", sub_inst);
-					// Use read value
-					sub_inst->setOperand(i, restore_ret);
+				    }
+				} else { // Return value is used in a non-phi instruction
+				    // Read the global variable
+				    LoadInst *restore_ret = new LoadInst(gvar, "", user_inst);
+				    // Find the uses of return value and replace them
+				    for (unsigned int i = 0; i < user_inst->getNumOperands(); i++) {
+					if (user_inst->getOperand(i) == call_func) { 
+					    user_inst->setOperand(i, restore_ret);
+					}
 				    }
 				}
 			    }
